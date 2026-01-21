c7198406a03649fdac040cebafef87a30ad5c64e
Vaibhav V <vaibhavprem24@gmail.com>
2026-01-21 15:57:41 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_URL || '/Weather-app20',
 })
